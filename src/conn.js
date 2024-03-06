f5ba2da6d285894f982915fa00edc61c6f205fc1
SergioAle210 <ore221122@uvg.edu.gt>
2024-03-05 21:01:56 -0600

    fix: change name database

--- src/conn.js
+++ src/conn.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2/promise'
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'sergio',
-  database: 'basketball_blog_db',
+  database: 'blog_sergio',
   password: 'serch',
   waitForConnections: true,
   connectionLimit: 10,
